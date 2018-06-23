5b27600876dba4dbc0fc830596aa039f8740348a
RJ <randalljo@gmail.com>
2018-06-23 10:13:46 -0600

    Initial commit 💥

---
+++ app/src/main/java/com/sweetbytes/camera2/Camera2Activity.kt
@@ -0,0 +1,12 @@
+package com.sweetbytes.camera2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Camera2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera2)
+    }
+}
